ca724ce29b8ef64f8ff850bfd7823c581e74d196
Vinisha Sukameti <vinisha@haulynx.com>
2020-03-26 20:31:09 -0700

    Added Data Pre-processing files

---
+++ Find_Accuracies.py
@@ -0,0 +1,39 @@
+from Model_Classification import ModelClassification
+from Data_Preprocess import get_split_data,get_in_scope_data,get_out_of_scope
+import numpy as np
+
+
+
+''' Getting the data which is extracted using the Function defined in DataPreprocess.py '''
+
+def predictions(dataset,model):
+
+
+
+    train_df,val_df,test_df,inscope_train,inscope_val,inscope_test = get_split_data(dataset)
+
+
+    if model == 'MLP':
+
+        """ Training the data on 'MLP' Classifier """
+
+        MLP_inscope = ModelClassification(model, train_df, test_df, val_df)
+        val_accuracy, test_accuracy, val_recall, test_recall = MLP_inscope.get_metrics_for_model()
+        print("Validation in-scope Accuracies for {} across {} is {}".format(model,dataset,val_accuracy))
+        print("Test in-scope Accuracies for {} across {} is {}".format(model,dataset,test_accuracy))
+
+
+        print("Validation Out_of_scope Recall for {} across {} is {}".format(model,dataset,val_recall))
+        print("Test Out_of_scope Recall for {} across {} is {}".format(model,dataset,test_recall))
+
+
+if __name__ == '__main__':
+
+    datasets = ['full','small','imbal','oos+']
+
+    # for data in datasets:
+    predictions('small','MLP')
+
+
+
+
